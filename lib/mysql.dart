c61cadbe1608df7f15149b8ec21c990689956940
mezha <mezhalee@gmail.com>
2022-04-27 00:43:58 +0300

    Fix server connection

--- lib/mysql.dart
+++ lib/mysql.dart
@@ -1,10 +1,16 @@
 import 'package:mysql1/mysql1.dart';
 
 class MySQL {
-  static String host = '10.0.2.2',
+  /*static String host = '10.0.2.2',
                 user = 'root',
                 password = 'root',
                 db = 'qrcode_webapp';
+  static int port = 3306;*/
+
+  static String host = 'sql11.freemysqlhosting.net',
+      user = 'sql11487689',
+      password = 'GfpLTs1PCl',
+      db = 'sql11487689';
   static int port = 3306;
 
   MySQL();
